0d362caeccb07fc20d1d2a44fe3564e3de09a35c
William_Tsai <tsaiyuhlin@gmail.com>
2018-01-22 19:21:20 -0800

    Initial Commit

---
+++ binary-counterTests/binary_counterTests.swift
@@ -0,0 +1,36 @@
+//
+//  binary_counterTests.swift
+//  binary-counterTests
+//
+//  Created by William Tsai on 1/22/18.
+//  Copyright © 2018 William Tsai. All rights reserved.
+//
+
+import XCTest
+@testable import binary_counter
+
+class binary_counterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
